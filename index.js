8ab9e023fa51451c71bb97dfc21cd2c920702e94
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-10-05 16:46:25 +0630

    added quotes

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ class TextGenerator extends React.Component {
 
     render () {
         const quotes = [
-            {quote: "If your dream don't scare you, they aren't big enough.", author: "Mohamad Ali"}
+            {
+                quote: "If your dream don't scare you, they aren't big enough.", author: "Mohamad Ali"
+            }, {
+                quote: "Persistence is the most travelled path to success.", author: "Bryant H. McGill"
+            }, {
+                quote: "Don't worry if you're not where you want to be yet. Great things take time.", author: "Bryant H. McGill"
+            }, {
+                quote: "Stay away from negative people. They have a problem for every solution.", author: "Albert Einstein"
+            }, {
+                quote: "A ship is always safe at shore but that is not what it's built for.", author: " Albert Einstein"
+            }, {
+                quote: "If things are not failing, you are not innovating enough.", author: "Elon Musk"
+            }, {
+                quote: "Bees don't waste their time explaining to flies that honey is better than shit.", author: "Jim Carrey's tweet"
+            }, {
+                quote: "Win in silence and let them think you are losing.", author: "Lil Tjay"
+            }, {
+                quote: "Talk sense to a fool and he calls you foolish.", author: "Eutipides"
+            }, {
+                quote: "Live for the praise of others and you will become their prisoner.", author: "Lao Tzu"
+            }, {
+                quote: "Who knows doesn't say. Who speaks doesn't know.", author: "Lao Tzu"
+            }, {
+                quote: "Everyone must choose one of two pains: The pain of discipline or the pain of regret.", author: "Jim John"
+            }
         ];
+        const randomQuote = Math.floor(Math.random()*13);
+
         return (
             <div>
             <div id="text">
-             {quotes[0].quote}
+             <p>{quotes[9].quote}</p>
+             <p>{randomQuote} </p>
             </div>
             <div id="author">
-                {quotes[0].author}
+                {quotes[9].author}
             </div>
             </div>
         )
